3262db2d6503c7befdb31f884e65e6037ec03af8
Muhab Seif <mohab.seif.16@gmail.com>
2022-06-11 00:27:34 +0000

    Finish fetch and React hooks

--- blango/settings.py
+++ blango/settings.py
@@ -259,7 +259,7 @@ class Dev(Configuration):
     },
     
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-        "PAGE_SIZE": 2,
+        "PAGE_SIZE": 5,
 
     "DEFAULT_FILTER_BACKENDS": [
             "django_filters.rest_framework.DjangoFilterBackend",
